9c65cb64e8554fc2de23b934cefc14a0e16d5e9d
Yuri Panchul <yuri@panchul.com>
2018-12-27 22:11:45 -0900

    Debugged the script

--- dff_tb/simulate_and_view_waveforms.bash
+++ dff_tb/simulate_and_view_waveforms.bash
@@ -1,81 +1,110 @@
 #!/bin/bash
 
-if command -v vsim &> /dev/null ; then
-    echo vsim
-fi
-
-if command -v iverilog &> /dev/null ; then
-    echo iverilog
-fi
-
-if command -v gtkwave &> /dev/null ; then
-    echo gtkwave
-fi
-
-if command -v iverilog &> /dev/null && command -v gtkwave  &> /dev/null ; then
-    echo iverilog and gtkwave
-fi
-
-exit
-
 set +e
 
-prepare_simulation_directory()
+#-----------------------------------------------------------------------------
+
+prepare_simulation_directory ()
 {
-if ! { rm -rf sim && mkdir sim && cd sim ; }; then
-	echo $0: Cannot prepare simulation directory
-	exit 1
-fi
+    if ! { rm -rf sim && mkdir sim && cd sim ; }; then
+        echo $0: Cannot prepare simulation directory
+        exit 1
+    fi
 }
 
-if ! iverilog -g2005 -I .. ../*.v &> compile.log ; then
-	echo $0: Verilog compiler returned error code
-	grep -i -A 5 error compile.log
-	exit 1
-fi
+#-----------------------------------------------------------------------------
 
-if ! vvp a.out &> simulate.log; then
-	echo $0: Verilog simulator returned error code
-	grep -i -A 5 error simulate.log
-	tail -n 5 simulate.log
-	exit 1
-fi
+is_icarus_verilog_available ()
+{
+    command -v iverilog &> /dev/null
+}
 
-echo $0: Simulation successfull, starting waveform viewer
-tail -n 5 simulate.log
+#-----------------------------------------------------------------------------
 
-if    [ "$OSTYPE" = "linux-gnu" ]  \
-   || [ "$OSTYPE" = "cygwin"    ]  \
-   || [ "$OSTYPE" = "msys"      ]; then
-
-    if ! gtkwave --dump dump.vcd --script ../script_for_gtkwave.tcl  \
-             &> waveform.log ; then
-	echo $0: Waveform viewer returned error code
-	grep -i -A 5 error waveform.log
-	exit 1
+simulate_icarus_verilog ()
+{
+    if ! iverilog -g2005 -I .. ../*.v &> compile.log ; then
+        echo $0: Verilog compiler returned error code
+        grep -i -A 5 error compile.log
+        exit 1
     fi
 
-elif [ ${OSTYPE/[0-9]*/} = "darwin" ]; then
-# elif [[ "$OSTYPE" = "darwin"* ]]; then  # Alternative way
-
-	# For some reason the following way of opening the application
-	# does not read the script file:
-    #
-    # open -a gtkwave dump.vcd --args --script $PWD/../script_for_gtkwave.tcl
-    #
-    # This way works:
-
-    if ! /Applications/gtkwave.app/Contents/MacOS/gtkwave-bin  \
-             --dump dump.vcd --script ../script_for_gtkwave.tcl  \
-             &> waveform.log ; then
-	echo $0: Waveform viewer returned error code
-	grep -i -A 5 error waveform.log
-	exit 1
+    if ! vvp a.out &> simulate.log; then
+        echo $0: Verilog simulator returned error code
+        grep -i -A 5 error simulate.log
+        tail -n 5 simulate.log
+        exit 1
     fi
 
-else
-    echo "Don't know how to run GTKWave on your OS"
-    exit 1
+    echo $0: Simulation successfull
+    tail -n 5 simulate.log
+}
+
+#-----------------------------------------------------------------------------
+
+is_gtkwave_viewer_available ()
+{
+    command -v gtkwave &> /dev/null
+}
+
+#-----------------------------------------------------------------------------
+
+run_gtkwave_viewer ()
+{
+    if    [ "$OSTYPE" = "linux-gnu" ]  \
+       || [ "$OSTYPE" = "cygwin"    ]  \
+       || [ "$OSTYPE" = "msys"      ]
+    then
+        gtkwave                                 \
+            --dump dump.vcd                     \
+            --script ../script_for_gtkwave.tcl  \
+            &> waveform.log
+
+    elif [ ${OSTYPE/[0-9]*/} = "darwin" ]
+    # elif [[ "$OSTYPE" = "darwin"* ]]  # Alternative way
+    then
+        # For some reason the following way of opening the application
+        # does not read the script file:
+        #
+        # open -a gtkwave dump.vcd --args --script $PWD/../script_for_gtkwave.tcl
+        #
+        # This way works:
+
+        /Applications/gtkwave.app/Contents/MacOS/gtkwave-bin    \
+            --dump dump.vcd --script ../script_for_gtkwave.tcl  \
+            &> waveform.log
+    else
+        echo "Don't know how to run GTKWave on your OS"
+        exit 1
+    fi
+
+    rc=$?
+
+    if [ $rc -ne 0 ]
+    then
+        echo $0: Waveform viewer returned error code $rc
+        grep -i -A 5 error waveform.log
+        exit 1
+    fi
+}
+
+#-----------------------------------------------------------------------------
+
+cleanup ()
+{
+    cd .. && rm -rf sim
+}
+
+#-----------------------------------------------------------------------------
+
+prepare_simulation_directory
+
+if is_icarus_verilog_available ; then
+    simulate_icarus_verilog
 fi
 
-cd .. && rm -rf sim
+if is_gtkwave_viewer_available ; then
+    run_gtkwave_viewer
+fi
+
+cleanup
---
+++ dff_tb/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ls lihlkhlk
+echo $?
+
+if true
+then
+    echo TRUE
+    echo $?
+    ls dddd
+fi
+
+echo $?
+ls *
+echo $?
