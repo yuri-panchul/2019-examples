ffd1764b47fe49ca5fca843f3269de200dd1972c
Yuri Panchul <yuri@panchul.com>
2019-04-03 20:23:25 -0700

    Test modification of ModelSim script

--- dff_tb/script_for_modelsim.tcl
+++ dff_tb/script_for_modelsim.tcl
@@ -1,5 +1,6 @@
 vlib work
 vlog -vlog01compat +define+USE_STOP_INSTEAD_OF_FINISH +incdir+.. ../*.v
+vlog -sv +incdir+/home/panchul/intelFPGA_lite/18.1/modelsim_ase/verilog_src/uvm-1.2/src /home/panchul/intelFPGA_lite/18.1/modelsim_ase/verilog_src/uvm-1.2/src/uvm.sv
 vsim work.tb
 add wave sim:/tb/*
 run -all
