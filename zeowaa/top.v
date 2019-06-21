a827f79b21a8366d04e810382444dc553b6337fc
Yuri Panchul <yuri@panchul.com>
2019-06-22 01:07:14 +0300

    Updated ZEOWAA wrapper with random starfield changes

--- zeowaa/top.v
+++ zeowaa/top.v
@@ -136,11 +136,29 @@ module top
         .vpos       ( vpos       )
     );
 
-    assign rgb = hpos ==  0 || hpos == 639 || vpos ==  0 || vpos == 479 ? 3'b100 :
-                 hpos ==  5 || hpos == 634 || vpos ==  5 || vpos == 474 ? 3'b010 :
-                 hpos == 10 || hpos == 629 || vpos == 10 || vpos == 469 ? 3'b001 :
-                 hpos <  20 || hpos >  619 || vpos <  20 || vpos >= 459 ? 3'b000 :
-                 { hpos [4], vpos [4], hpos [3] ^ vpos [3] };
+    wire [2:0] rgb_squares
+        = hpos ==  0 || hpos == 639 || vpos ==  0 || vpos == 479 ? 3'b100 :
+          hpos ==  5 || hpos == 634 || vpos ==  5 || vpos == 474 ? 3'b010 :
+          hpos == 10 || hpos == 629 || vpos == 10 || vpos == 469 ? 3'b001 :
+          hpos <  20 || hpos >  619 || vpos <  20 || vpos >= 459 ? 3'b000 :
+          { hpos [4], vpos [4], hpos [3] ^ vpos [3] };
+
+    wire        lfsr_enable = ! hpos [9:8] & ! vpos [9:8];
+    wire [15:0] lfsr_out;
+
+    lfsr #(16, 16'b1000000001011, 0) i_lfsr
+    (
+        .clk    ( clk         ),
+        .reset  ( reset       ),
+        .enable ( lfsr_enable ),
+        .out    ( lfsr_out    )
+    );
+
+    wire star_on = & lfsr_out [15:9];
+
+    wire [2:0] rgb = lfsr_enable ?
+                         (star_on ? lfsr_out [2:0] : 3'b0)
+                       : rgb_squares;
 
     //------------------------------------------------------------------------
 
