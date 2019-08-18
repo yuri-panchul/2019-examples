c88e1c79ab07f3b1af8154a381bc77050be13bf7
Yuri Panchul <yuri@panchul.com>
2019-08-18 15:43:25 -0700

    Started creating scripting examples for the illustration of Bash, Perl, Tcl, Python scripting

---
+++ scripting/babble.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+a=( "Palo Alto"     \
+    "Fremont"       \
+    "Mountain View" \
+    "Milpitas"      \
+    "Sunnyvale"     \
+    "Santa Clara"   \
+    "Los Altos"     \
+    "Cupertino"     \
+    "San Jose"      \
+    "Saratoga"      \
+    "Campbell"      \
+    "Los Gatos"     )
+
+print_array ()
+{
+    printf "$*\n"
+    printf '    %s\n' "${a[@]}"
+}
+
+print_array Before sort
+
+run=true
+
+while $run
+do
+    run=false
+
+    for i in $(seq 0 $(( ${#a[@]} - 2)) )
+    do
+        i_1=$(( $i + 1 ))
+        
+        if [ "${a[$i_1]}" \< "${a[$i]}" ]
+        then
+            t=${a[$i]}
+            a[$i]=${a[$i_1]}
+            a[$i_1]=$t
+            
+            run=true
+        fi
+    done
+
+done
+
+print_array After sort
