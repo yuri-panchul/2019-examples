bc1fbe14b78da80bfd4f03bb3272a3ee9541a1a8
Yuri Panchul <yuri@panchul.com>
2019-08-20 21:48:45 -0700

    More compact coded arbiter

--- arbiters/05_rotate_priority_rotate_brief/arbiter.v
+++ arbiters/05_rotate_priority_rotate_brief/arbiter.v
@@ -6,40 +6,20 @@ module arbiter
     output reg [7:0] gnt
 );
 
-    reg [2:0] ptr;
+    reg  [ 2:0] ptr;
 
     wire [15:0] shift_req_double = { req, req } >> ptr;
     wire [ 7:0] shift_req = shift_req_double [7:0];
 
-    reg [7:0] d_gnt;
+    wire [ 7:0] higher_pri_reqs;
 
-    // Priority arbiter using if
+    assign      higher_pri_reqs [7:1] = higher_pri_reqs [6:0] | shift_req [6:0];
+    assign      higher_pri_reqs [0]   = 0;
 
-    always @*
-    begin
-        shift_gnt = 8'b0;
+    wire [ 7:0] shift_gnt = shift_req & ~ higher_pri_reqs;
 
-             if (shift_req [0]) shift_gnt [0] = 1;
-        else if (shift_req [1]) shift_gnt [1] = 1;
-        else if (shift_req [2]) shift_gnt [2] = 1;
-        else if (shift_req [3]) shift_gnt [3] = 1;
-        else if (shift_req [4]) shift_gnt [4] = 1;
-        else if (shift_req [5]) shift_gnt [5] = 1;
-        else if (shift_req [6]) shift_gnt [6] = 1;
-        else if (shift_req [7]) shift_gnt [7] = 1;
-    end
-
-    always @*
-        case (ptr)
-        3'd0: d_gnt =   shift_gnt [7:0]                   ;
-        3'd1: d_gnt = { shift_gnt [6:0], shift_gnt [7:7] };
-        3'd2: d_gnt = { shift_gnt [5:0], shift_gnt [7:6] };
-        3'd3: d_gnt = { shift_gnt [4:0], shift_gnt [7:5] };
-        3'd4: d_gnt = { shift_gnt [3:0], shift_gnt [7:4] };
-        3'd5: d_gnt = { shift_gnt [2:0], shift_gnt [7:3] };
-        3'd6: d_gnt = { shift_gnt [1:0], shift_gnt [7:2] };
-        3'd7: d_gnt = { shift_gnt [0:0], shift_gnt [7:1] };
-        endcase
+    wire [15:0] d_gnt_double = { shift_gnt, shift_gnt } << ptr;
+    wire [ 7:0] d_gnt = d_gnt_double [15:8];
 
     always @ (posedge clk or posedge rst)
         if (rst)
