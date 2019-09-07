98542f293c738b7649050399c7c865f8ed70d237
Yuri Panchul <yuri@panchul.com>
2019-09-07 12:17:44 -0700

    Improving scripts and makefiles

--- cdc_fifo_arbiters/cdc/03_sync_enable_using_toggle/r.bat
+++ cdc_fifo_arbiters/cdc/03_sync_enable_using_toggle/r.bat
@@ -1,8 +1,1 @@
-rd /s /q sim
-md sim
-cd sim
-
-\iverilog\bin\iverilog -g2005 ../*.v
-\iverilog\bin\vvp a.out > a.lst
-type a.lst
-\iverilog\gtkwave\bin\gtkwave.exe dump.vcd -S ../gtkwave.tcl
+../../bin/r.bat
