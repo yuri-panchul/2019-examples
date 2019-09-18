8abe4db7f897108c2b25d7e1be3fe49a64cdadb0
Yuri Panchul <yuri@panchul.com>
2019-09-17 20:43:36 -0700

    minor changes

--- next_step_examples/01_simple/01_gates_d_latch_and_dff_using_mux/gtkwave.tcl
+++ next_step_examples/01_simple/01_gates_d_latch_and_dff_using_mux/gtkwave.tcl
@@ -9,11 +9,6 @@ lappend all_signals tb.o
 lappend all_signals tb.e
 lappend all_signals tb.oxe
 
-lappend all_signals tb.a
-lappend all_signals tb.b
-lappend all_signals tb.f
-lappend all_signals tb.i_dff_q
-
 set num_added [ gtkwave::addSignalsFromList $all_signals ]
 
 gtkwave::/Time/Zoom/Zoom_Full
